1ef5029088b52adeddbca17a45018094c692a4c0
ThomasNisterenko <70923469+ThomasNisterenko@users.noreply.github.com>
2021-10-09 17:14:33 -0300

    Create run.py
    Create script run.py

---
+++ run.py
@@ -0,0 +1,24 @@
+#!usr/bin/env python3
+
+"""
+Author: Thomas Cascais Nisterenko
+Date: 2021-10-09
+Purpose: This python script takes user reviews written in .txt files
+         inside a directory, parses the contents of the files in
+         dictionaries and posts them to a website.
+"""
+
+import os
+import requests
+
+
+def main():
+    """
+    main function that takes care of the program's overall logic and calls
+    other functions.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
