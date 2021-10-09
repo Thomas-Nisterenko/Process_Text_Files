83baefbaf388673fe6e2427bbe9d1b3b54fe4ba5
ThomasNisterenko <70923469+ThomasNisterenko@users.noreply.github.com>
2021-10-09 17:21:02 -0300

    Update run.py
    Adds function stubs to run.py script

--- run.py
+++ run.py
@@ -12,6 +12,32 @@ import os
 import requests
 
 
+def get_txtfiles(dir_path):
+    """
+    This function gets all files from a sepcified directory
+    and produces a list with them.
+    :param dir_path: string with path to directory
+    :return txtfiles: list with  filenames of txt files
+    """
+
+
+def file_to_dict(filename):
+    """
+    This function takes a filename, opens its associated file,
+    and produces a dictionary with the file's information.
+    :param filename: string with the file's name
+    :return file_dict: dictionary formatted with file information
+    """
+
+def post_dict(review_dict, address):
+    """
+    This function takes a dictionary and posts its contents to the webpage.
+    :param review_dict: dictionary with review information
+    :param address: string with the webpage's address
+    :return: None
+    """
+
+
 def main():
     """
     main function that takes care of the program's overall logic and calls
